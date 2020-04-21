b5b7a1a45a46d331d22e5282e4daf07020890419
yang <825025679@qq.com>
2020-04-21 14:36:28 +0800

    宠物管理系统

--- src/main/java/cn/blb/boot/controller/AnimalController.java
+++ src/main/java/cn/blb/boot/controller/AnimalController.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- * @author Ironhide
- * @create 2020-04-20-18:02
+ * 用的是tkmybatis提供的的方式进行crud
  */
 @RequestMapping("animal")
 @Controller
@@ -20,11 +19,19 @@ public class AnimalController {
     @Autowired
     private AnimalMapper mapper;
 
+    /**
+     * 进入主页面
+     * @return
+     */
     @GetMapping("select")
     public String toListPage() {
         return "animals";
     }
 
+    /**
+     * 查询
+     * @return
+     */
     @ResponseBody
     @GetMapping("list")
     public List<Animal> list() {
@@ -32,12 +39,22 @@ public class AnimalController {
         return animals;
     }
 
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
     @ResponseBody
     @GetMapping("delete/{id}")
     public Integer deleteAnimal(@PathVariable("id") Integer id) {
         return mapper.deleteByPrimaryKey(id);
     }
 
+    /**
+     * 带id就执行修改，否则是增加
+     * @param animal
+     * @return
+     */
     @PostMapping("saveOrUpdate")
     public String insert(Animal animal) {
         if (animal.getId() != null) {
@@ -48,6 +65,13 @@ public class AnimalController {
         return "animals";
     }
 
+    /**
+     * 根据id查询 然后把数据带去修改页面
+     *
+     * @param id
+     * @param model
+     * @return
+     */
     @GetMapping("toUpdatePage")
     public String toUpdatePage(Integer id, Model model) {
         model.addAttribute("animal", mapper.selectByPrimaryKey(id));
